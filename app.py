8ff917d0d74473f9d15b21f3037dbdaf30d84d89
Hibandd122 <130156106+Hibandd122@users.noreply.github.com>
2025-08-07 12:59:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Bật CORS cho tất cả domain
 
 # Hàm load file mỗi lần gọi API
 def load_results(file_path='results.txt'):
